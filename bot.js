f1bc08c902a8ed01ed6f2ee9f63d39ecfb3f68a7
tooreagen <tooreagen@gmail.com>
2023-11-02 09:58:22 +0200

    process...

--- bot.js
+++ bot.js
@@ -12,16 +12,16 @@ const setupBot = () => {
     next();
   });
 
-  // bot.on("message", (ctx) => {
-  //   if (ctx.message.text === "1") {
-  //     ctx.reply("Можна декілька разів робити bot.on якщо викликати next");
-  //   }
-  // });
+  bot.hears("привет", (ctx) => ctx.reply("та написав привет"));
+  bot.hears("1", (ctx) => ctx.reply("та написав один"));
+  bot.hears("рок", (ctx) => ctx.reply("рок-н-рол"));
 
-  bot.hears('привет', (ctx) => ctx.reply('та написав привет'))
-  bot.hears('1', (ctx) => ctx.reply('та написав один'))
-  bot.hears('рок', (ctx) => ctx.reply('рок-н-рол'))
-
+  bot.use(async (ctx) => {
+    await ctx.reply(
+      "Что нужно сделать?",
+      Markup.keyboard([["Авторизація", "Налаштування"]]).resize()
+    );
+  });
 
   return bot;
 };
@@ -33,3 +33,55 @@ process.once("SIGTERM", () => bot.stop("SIGTERM"));
 module.exports = { setupBot };
 
 //bot.on("message") — он запускается каждый раз, когда кто-то отправляет сообщение.
+
+// const setupBot = () => {
+
+//   //   bot.command("help", (ctx) => {
+//   //     ctx.reply("Ви запросили допомогу! Я вам допоможу. ");
+//   //   });
+
+//   //   bot.command("settings", (ctx) => {
+//   //     ctx.reply("Ви запросили налаштування. ");
+//   //   });
+
+//   const getCoinSide = () => (getRandomInt(0, 1) === 0 ? "Орёл" : "Решка");
+//   const coinInlineKeyboard = Markup.inlineKeyboard([
+//     Markup.button.callback("Подбросить ещё раз", "flip_a_coin"),
+//     Markup.button.callback("Еще кнлпочка", "1"),
+//   ]);
+//   bot.hears("Подбросить монетку", (ctx) => ctx.reply(getCoinSide(), coinInlineKeyboard));
+//   //Подбросить ещё раз
+//   bot.action("flip_a_coin", async (ctx) => {
+//     await ctx.editMessageText(
+//       `${getCoinSide()}\nОтредактировано: ${new Date().toISOString()}`,
+//       coinInlineKeyboard
+//     );
+//   });
+
+//   const getRandomNumber = () => getRandomInt(0, 3);
+//   const numberInlineKeyboard = Markup.inlineKeyboard([
+//     Markup.button.callback("Сгенерировать новое  инлайн", "random_number"),
+//   ]);
+//   bot.hears("Случайное число", (ctx) =>
+//     ctx.reply(getRandomNumber().toString(), numberInlineKeyboard)
+//   );
+//   bot.action("random_number", async (ctx) => {
+//     await ctx.editMessageText(
+//       `${getRandomNumber()}\nОтредактировано: ${new Date().toISOString()}`,
+//       numberInlineKeyboard
+//     );
+//   });
+
+//   bot.use(async (ctx) => {
+//     await ctx.reply(
+//       "Что нужно сделать?",
+//       Markup.keyboard([["Подбросить монетку", "Случайное число"]]).resize()
+//     );
+//   });
+
+//   return bot;
+// };
+
+// //bot.on("message") — он запускается каждый раз, когда кто-то отправляет сообщение.
+// //Через ctx.reply мы отвечаем в тот же чат, а через ctx.telegram.sendMessage(chat_id, `Hello`) можно отправить сообщение в произвольный чат.
+// //hears — регистрирует middleware, которые реагируют на текстовые сообщения указанного содержания
