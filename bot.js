ede8594ba9b6ea10b0f254b97c53b8cf1244207f
tooreagen <tooreagen@gmail.com>
2023-11-02 10:14:34 +0200

    process...

--- bot.js
+++ bot.js
@@ -1,26 +1,81 @@
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const setupBot = () => {
-  bot.start((ctx) => ctx.reply("Вітаю!"));
+  // bot.start((ctx) => ctx.reply("Вітаю!"));
 
-  bot.on("message", async (ctx, next) => {
-    const chatID = ctx.chat.id;
-    ctx.reply("chatID: " + chatID);
-    next();
+  // bot.on("message", async (ctx, next) => {
+  //   const chatID = ctx.chat.id;
+  //   ctx.reply("ID чата: " + chatID);
+  //   next();
+  // });
+
+  // // bot.hears("привет", (ctx) => ctx.reply("та написав привет"));
+  // // bot.hears("1", (ctx) => ctx.reply("та написав один"));
+  // // bot.hears("рок", (ctx) => ctx.reply("рок-н-рол"));
+
+  // const getCoinSide = () => (getRandomInt(0, 1) === 0 ? "Орёл" : "Решка");
+
+  // const coinInlineKeyboard = Markup.inlineKeyboard([
+  //   Markup.button.callback("Подбросить ещё раз", "flip_a_coin"),
+  //   Markup.button.callback("Еще кнлпочка", "1"),
+  // ]);
+
+  // bot.hears("Авторизація", (ctx) => ctx.reply(getCoinSide(), coinInlineKeyboard));
+
+  // //Подбросить ещё раз
+  // bot.action("flip_a_coin", async (ctx) => {
+  //   await ctx.editMessageText(
+  //     `${getCoinSide()}\nОтредактировано: ${new Date().toISOString()}`,
+  //     coinInlineKeyboard
+  //   );
+  // });
+
+  // const getRandomNumber = () => getRandomInt(0, 3);
+
+  // const numberInlineKeyboard = Markup.inlineKeyboard([
+  //   Markup.button.callback("Сгенерировать новое  инлайн", "random_number"),
+  // ]);
+
+  // bot.hears("Налаштування", (ctx) => ctx.reply(getRandomNumber().toString(), numberInlineKeyboard));
+
+  // bot.action("random_number", async (ctx) => {
+  //   await ctx.editMessageText(
+  //     `${getRandomNumber()}\nОтредактировано: ${new Date().toISOString()}`,
+  //     numberInlineKeyboard
+  //   );
+  // });
+
+  // bot.use(async (ctx) => {
+  //   await ctx.reply(
+  //     "Что нужно сделать?",
+  //     Markup.keyboard([["Авторизація", "Налаштування"]]).resize()
+  //   );
+  // });
+
+  bot.command("dynamic-buttons", (ctx) => {
+    return ctx.reply(
+      "Dynamic buttons example.",
+      Markup.inlineKeyboard([
+        Markup.callbackButton("Option 1", "option1"),
+        Markup.callbackButton("Option 2", "option2"),
+        Markup.callbackButton("Option 3", "option3"),
+      ]).extra()
+    );
   });
 
-  bot.hears("привет", (ctx) => ctx.reply("та написав привет"));
-  bot.hears("1", (ctx) => ctx.reply("та написав один"));
-  bot.hears("рок", (ctx) => ctx.reply("рок-н-рол"));
+  bot.action("option1", (ctx) => {
+    return ctx.answerCbQuery("Option 1 selected!");
+  });
 
-  bot.use(async (ctx) => {
-    await ctx.reply(
-      "Что нужно сделать?",
-      Markup.keyboard([["Авторизація", "Налаштування"]]).resize()
-    );
+  bot.action("option2", (ctx) => {
+    return ctx.answerCbQuery("Option 2 selected!");
+  });
+
+  bot.action("option3", (ctx) => {
+    return ctx.answerCbQuery("Option 3 selected!");
   });
 
   return bot;
