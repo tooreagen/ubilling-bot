679e8d5a9b0c5943a493f3089c340e9fa1fc9544
tooreagen <tooreagen@gmail.com>
2023-11-02 18:57:12 +0200

    education

--- keyboards.js
+++ keyboards.js
@@ -1,6 +1,7 @@
 import Markup from "telegraf/markup.js";
 
 export function getMainMenu() {
+  console.log("Keyboard");
   return Markup.keyboard([["Мои задачи", "Добавить задачу"], ["Смотивируй меня"]])
     .resize()
     .extra();
